0d7409e596411470fc46785e92dce2060a6096b6
Kurt <52134197+KurtHilder@users.noreply.github.com>
2021-02-05 13:01:06 +0200

    uploading things

--- IT_CODE/TestCode.java
+++ IT_CODE/TestCode.java
@@ -1,5 +1,8 @@
+import it.Gogga;
+
 public class TestCode {
    public static void main(String[]args){
+      Gogga test = new Gogga();
       System.out.println("jGrasp is working");
       char space= 37;
       char a = 64;
---
+++ IT_CODE/TestGogga.java
@@ -0,0 +1,49 @@
+import it.Gogga;
+
+public class TestGogga{
+   public static void main(String[] args){
+      Gogga.setGridSize(20,20);
+      
+      Gogga bug = new Gogga();
+   
+      int initPos = 9;
+      
+      bug.setPosition(9,9);
+      bug.setDirection(1);
+      bug.move();
+      
+      bug.setDirection(4);
+      bug.move();
+   
+      bug.setDirection(2);
+      bug.move();
+      bug.move();
+      
+      bug.setDirection(3);
+      bug.move();
+      bug.move();
+      
+      bug.setDirection(1);
+      bug.move();
+      bug.move();
+      bug.move();
+      
+      bug.setDirection(4);
+      bug.move();
+      bug.move();
+      bug.move();
+      
+      bug.setDirection(2);
+      bug.move();
+      bug.move();
+      bug.move();
+      bug.move();
+      
+      bug.setDirection(3);
+      bug.move();
+      bug.move();
+      bug.move();
+      bug.move();
+      
+   }
+}
