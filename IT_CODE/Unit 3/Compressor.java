fdd2b172132c021c03b2b6d7515d662afd85af5d
Kurt <52134197+KurtHilder@users.noreply.github.com>
2021-05-28 11:51:07 +0200

    last upload before exams

---
+++ IT_CODE/Unit 3/Compressor.java
@@ -0,0 +1,25 @@
+public class Compressor{
+   public static void main(String[] args){
+     
+      // convert this too a string and print it to the console.(1)
+      int start = 145887769;
+      String startInt = Integer.toString(start);
+      System.out.println(startInt);
+      
+      
+      int num = 3;
+      
+      if(num == 2){
+        System.out.println("Its two");
+      } 
+      if (num != 2) {
+        System.out.println("Its not two");
+      } 
+      if(num != 1) {
+        System.out.println("Its also not two");
+      }
+      
+      
+      
+   }
+}
---
+++ IT_CODE/Unit 3/JustWantedToShowYou.java
@@ -0,0 +1,15 @@
+// Calculator 
+// Your Name
+
+import javax.swing.*;
+
+public class JustWantedToShowYou{
+
+   public static void main(String[] args){
+   
+      String hello = JOptionPane.showInputDialog("Type some stuff");
+      // String hello = "x=4/5";
+      System.out.println(hello);
+      
+   }
+}
