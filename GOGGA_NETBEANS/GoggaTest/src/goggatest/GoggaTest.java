4c99d98483b00bda8361d26f681e98b96584e0a6
KurtHilder <52134197+KurtHilder@users.noreply.github.com>
2021-10-15 14:12:41 +0200

    2021-10-15 Gogga Stuff

--- GOGGA_NETBEANS/GoggaTest/src/goggatest/GoggaTest.java
+++ GOGGA_NETBEANS/GoggaTest/src/goggatest/GoggaTest.java
@@ -3,19 +3,33 @@
  *  INFORMATION TECHNOLOGY
  *  CLASS NOTES
  */
-package usingsqlwithjava;
+package goggatest;
+import it.*;
 
 /**
  *
  * @author kwhil
  */
-public class UsingSQLwithJava {
+public class GoggaTest {
 
 	/**
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
 		// TODO code application logic here
+		Gogga bug = new Gogga();
+
+		bug.move();
+		bug.move();
+		bug.move();
+		bug.move();
+
+		bug.turnLeft();
+
+		bug.move();
+		bug.move();
+		bug.move();
+		bug.move();
 	}
 	
 }
